3c3092fcf9dddf14f8f1bef2d62f508573037b15
dev1@flairminds.com <dev1@flairminds.com>
2024-12-18 11:26:50 +0530

    Separated connection testing APIs

--- app/routes/connection_routes.py
+++ app/routes/connection_routes.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, jsonify
 import psycopg2
 
+from app.services.redis_services import search_in_malware_cache, search_in_white_cache, RedisService
 from config import Config
 
 test = Blueprint('test', __name__)
 pg_connection = Blueprint('postgres', __name__)
+redis_bp = Blueprint('redis', __name__)
+
+redis_service = RedisService()
 
 @test.route('/ping', methods=['GET'])
 def ping():
@@ -28,3 +32,7 @@ def test_db_connection():
         return jsonify({"status": "success", "message": "DB connection is working!", "query_result": result}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
+
+@redis_bp.route('/checkRedisConnection', methods=['GET'])
+def check_redis_connection_route():
+    return redis_service.check_redis_connection()
