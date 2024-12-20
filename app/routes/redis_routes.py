3f954e2d87da4721292333546e40b17cfb09492b
dev1@flairminds.com <dev1@flairminds.com>
2024-12-20 20:08:45 +0530

    condition added - if score > 0.4 then malicious else white

--- app/routes/redis_routes.py
+++ app/routes/redis_routes.py
@@ -59,7 +59,7 @@ def search_malicious_url():
         cached_result = redis_service.search_in_malicious_url_cache(md5_hash)
         if cached_result:
             vendor, score = cached_result.split('|')[2], cached_result.split('|')[1]
-            if score > 0.4:
+            if float(score) > 0.4:
                 return jsonify({"status": 2, "source": 1, "Vendor": vendor, "Score": score}), 200 
             else:
                 return jsonify({"status": 0}), 200 
