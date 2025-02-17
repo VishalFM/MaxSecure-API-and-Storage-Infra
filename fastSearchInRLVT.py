a0aea9e1aada56090983ff71c3ad30f2e6cda481
dev1@flairminds.com <dev1@flairminds.com>
2025-02-17 16:48:43 +0530

    RL VT Endpoint created

--- fastSearchInRLVT.py
+++ fastSearchInRLVT.py
@@ -273,8 +273,3 @@ async def fast_search_malicious_url(request: Request):
         total_time = time.time() - start_time
         print(f"Total Execution Time: {total_time:.4f} seconds")
         return JSONResponse({"status": 0, "error": f"Internal server error: {str(e)}"}, status_code=500)
-
-@app.post("/pingFast")
-async def search_endpoint(request: Request):
-    data = await request.json()
-    return {"message": "Search endpoint is working!", "data": data}
